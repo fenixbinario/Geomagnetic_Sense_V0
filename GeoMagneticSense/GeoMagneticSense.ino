963097f5881b96f1dee36b510fbd3faff1065102
Fenix <fbamperius@gmail.com>
2019-03-06 21:54:56 +0100

    Agregar archivos de proyecto.

---
+++ GeoMagneticSense/GeoMagneticSense.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		GeoMagneticSense.ino
+ Created:	06/03/2019 21:54:08
+ Author:	@fenixbinario	|	www.fenixbinario.com
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
